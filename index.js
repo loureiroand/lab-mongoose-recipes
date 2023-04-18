c7e3c57455148adf9e3758a408851b5d18536a49
loureiroand <124377344+loureiroand@users.noreply.github.com>
2023-04-18 18:55:44 +0100

    Completed Lab

--- index.js
+++ index.js
@@ -3,21 +3,62 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI =
+  'mongodb+srv://loureiroand:loureiroand@ironhack.t5nhc87.mongodb.net/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
+
+async function updateDatabase() {
+  // Connection to the database "recipe-app"
+  try {
+    const x = await mongoose.connect(MONGODB_URI);
+    console.log('connected to', x.connections[0].name);
+    await Recipe.deleteMany();
+
+    //Start coding here
+    const newRecipe = {
+      title: 'Cod à Brás',
+      level: 'Amateur Chef',
+      ingredients: ['cod', 'straw potatoes', 'onion', 'garlic', 'parsley'],
+      cuisine: 'Portuguese',
+      dishType: 'main_course',
+      duration: 60,
+      creator: 'Chef Meireles'
+    };
+
+    const createdRecipe = await Recipe.create(newRecipe);
+    console.log('createdRecipe:', createdRecipe);
+
+    const createdRecipes = await Recipe.insertMany(data);
+    console.log('createdRecipes', createdRecipes);
+    createdRecipes.forEach(recipe => console.log(recipe.title));
+
+    const filterRecipe = { title: 'Rigatoni alla Genovese' };
+    const update = { duration: 100 };
+    const options = { new: true };
+    const updatedRecipe = await Recipe.findOneAndUpdate(
+      filterRecipe,
+      update,
+      options
+    );
+    console.log('updatedRecipe:', updatedRecipe);
+
+    console.log(
+      `The '${updatedRecipe.title}' recipe has been updated with success`
+    );
+
+    const findRecipe = { title: 'Carot Cake' };
+    const removeRecipe = await Recipe.deleteOne(findRecipe);
+    console.log('removeRecipe:', removeRecipe);
+    console.log(`The Carrot Cake recipe has been removed!`);
+  } catch (error) {
     console.error('Error connecting to the database', error);
-  });
+  } finally {
+    mongoose.connection.close();
+    console.log('The connection to the database has been closed!');
+  }
+}
+
+updateDatabase();
